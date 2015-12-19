6a29925eb2fe5ba39a438f231eb0f40f788b996c
Michael Schatz <wusaaaaa@openmailbox.org>
2015-12-19 11:42:16 +0100

    Add Settings

---
+++ Epub-Manager/Epub-Manager/Views/Settings/SettingsView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using DevExpress.Xpf.Ribbon;
+using Epub_Manager.Views.Shell;
+using System.Windows.Controls;
+
+namespace Epub_Manager.Views.Settings
+{
+    /// <summary>
+    /// Interaction logic for SettingsView.xaml
+    /// </summary>
+    public partial class SettingsView : UserControl, IHaveRibbonToMerge
+    {
+        public SettingsViewModel ViewModel => this.DataContext as SettingsViewModel;
+        public RibbonControl RibbonControl => this.ActualRibbonControl;
+
+        public SettingsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Epub-Manager/Epub-Manager/Views/Settings/SettingsViewModel.cs
@@ -0,0 +1,67 @@
+﻿using Caliburn.Micro;
+using DevExpress.Mvvm;
+using Epub_Manager.Extensions;
+using Epub_Manager.Views.Shell;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.IO;
+using System.Windows.Input;
+
+namespace Epub_Manager.Views.Settings
+{
+    public class SettingsViewModel : Screen, IShellItem
+    {
+        private string _folderPath;
+
+        public string FolderPath
+        {
+            get { return this._folderPath; }
+            set { this.SetProperty(ref this._folderPath, value); }
+        }
+
+        public ICommand SaveCommand { get; }
+        public ICommand OpenFolderBrowserCommand { get; }
+
+        public SettingsViewModel()
+        {
+            this.DisplayName = "Settings";
+
+            this.SaveCommand = new DelegateCommand(this.Save);
+            this.OpenFolderBrowserCommand = new DelegateCommand(this.OpenFolderBrowser);
+        }
+
+
+        protected override void OnActivate()
+        {
+            var location = AppDomain.CurrentDomain.BaseDirectory + @"\Settings.ini";
+
+            if (!File.Exists(location))
+                return;
+
+            using (var reader = new StreamReader(location))
+                this.FolderPath = reader.ReadLineAsync().Result;
+        }
+
+        private void OpenFolderBrowser()
+        {
+            var folderDialog = new CommonOpenFileDialog
+            {
+                IsFolderPicker = true
+            };
+
+            var result = folderDialog.ShowDialog();
+
+            if (result == CommonFileDialogResult.Ok)
+                this.FolderPath = folderDialog.FileName;
+        }
+
+        private void Save()
+        {
+            var location = AppDomain.CurrentDomain.BaseDirectory + @"\Settings.ini";
+
+
+            using (var file = new StreamWriter(location))
+                file.WriteLine(this.FolderPath);
+        }
+    }
+}
